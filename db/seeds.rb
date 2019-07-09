e7b4bbda78b76c51dc73c7b5b81f379c580412e1
Mark <m_arch@outlook.com.au>
2019-07-09 12:31:42 +1000

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+course1 = Course.create(title: "CS111: Intro to Computer Science", description: "An in-depth look at computer science as a field an...")
+course2 = Course.create(title: "CS112: Data Structures", description: "An in-depth look at data structures and establishi...")
+course3 = Course.create(title: "CS205: Intro to Linear Algebra", description: "An essential Introduction to Linear Algebra and ho...")
